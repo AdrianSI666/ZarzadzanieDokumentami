bf9cdc38b624b54bfb4dc320dcd29adcb966543d
AdrianSI666 <19426@student.ans-elblag.pl>
2023-02-09 19:28:14 +0100

    Added static strings for paths.

---
+++ src/main/java/com/asledz/kancelaria_prawnicza/enums/Path.java
@@ -0,0 +1,11 @@
+package com.asledz.kancelaria_prawnicza.enums;
+
+public class Path {
+    private Path(){}
+    public static final String DOCUMENT_VALUE = "/document";
+    public static final String FILE_VALUE = "/file";
+    public static final String ROLE_VALUE = "/role";
+    public static final String TAG_VALUE = "/tag";
+    public static final String TYPE_VALUE = "/type";
+    public static final String USER_VALUE = "/user";
+}
